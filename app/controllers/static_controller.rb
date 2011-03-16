037add469dbedaebc92dce9ff914dfd0ce8d966f
Danidemi <demichelis@danidemi.com>
2011-03-16 06:50:59 +0100

    a selection of nice requests appear in the index

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,33 @@ class StaticController < ApplicationController
 
   def index
     @search = Struct.new("Search", :query, :category_key).new("","")
+
+    # find groups of interesting request that could be shown to the user.
+
+    @expiring_requests = Request \
+      .where('status = :status', :status => :active) \
+      .where(':now<expiration', :now => DateTime.now) \
+      .order("expiration ASC") \
+      .limit(3)
+
+    @newest_requests = Request \
+      .where('status = :status', :status => :active) \
+      .where(':now<expiration', :now => DateTime.now) \
+      .order("created_at DESC") \
+      .limit(3)
+
+    @uncared_requests = Request \
+      .joins(:proposals) \
+      .where('status = :status', :status => :active) \
+      .where(':now<expiration', :now => DateTime.now) \
+      .limit(3)
+
+    #This could be used to extract request without offers
+    #SELECT requests.*, count(proposals.request_id) FROM requests JOIN proposals on requests.id=proposals.request_id
+    #WHERE requests.status = 'active'
+    #GROUP BY proposals.request_id
+    #HAVING count(proposals.request_id) = 0
+      
   end
 
   def how_it_works
