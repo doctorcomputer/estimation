ed745e53aeb91938af37ad7783263488312ae475
danidemi <demichelis@danidemi.com>
2011-05-15 16:41:42 +0200

    added prod table prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Estimating::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.active_record.table_name_prefix = "miserve_"
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
